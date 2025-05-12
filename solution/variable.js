fcdcf78085927e2ca56112283a7eb1e1b692dd3a
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-05-12 15:02:31 +0600

    variable related problem solved

---
+++ solution/variable.js
@@ -0,0 +1,19 @@
+let age = 20;
+console.log(age);
+
+let bookToRead = 5;
+console.log(bookToRead);
+
+let wakeUpTime = 7;
+console.log(wakeUpTime);
+
+let wastedTime = 7;
+console.log(wastedTime);
+
+let borrow = 500;
+console.log(borrow);
+
+let goals = 4;
+
+let pillow = 3;
+console.log(pillow);
