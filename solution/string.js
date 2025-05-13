d5fb48e7e1ff74c6cdf75709c2430d5450bdc4cd
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-05-13 21:29:12 +0600

    practice problem solved

---
+++ solution/string.js
@@ -0,0 +1,20 @@
+const name = "Ashraful";
+console.log(name);
+
+const address = "Barishal, Bangladesh";
+console.log(address);
+
+const bestFriend = "Mobile and Laptop";
+console.log(bestFriend);
+
+const favoriteActress = "Uhu Lula La";
+console.log(favoriteActress);
+
+const movieOfTheYear = "Shahabaghi Maggi";
+console.log(movieOfTheYear);
+
+const hobby = "Coding";
+console.log(hobby);
+
+const favoriteColor = "Blue";
+console.log(favoriteColor);
